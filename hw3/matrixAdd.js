0c22e33899a597534ffdd8cf648e54198ea82bbc
HJH60 <116771258+HJH60@users.noreply.github.com>
2024-03-20 14:27:01 +0800

    Add files via upload

---
+++ hw3/matrixAdd.js
@@ -0,0 +1,12 @@
+function matrixAdd(a,b){
+    var i, j, c = []
+    for (i=0; i<a.length; i++) {
+        c[i] = []
+        for (j=0; j<a[i].length; j++) {
+            c[i][j] = a[i][j] + b[i][j]
+        }
+    }
+    return c
+}
+var a=[[1,2],[3,4]], b=[[5,6],[7,8]]
+console.log(matrixAdd(a,b))
